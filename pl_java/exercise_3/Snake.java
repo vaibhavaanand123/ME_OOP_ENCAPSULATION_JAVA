c405d693c8e34015aed1585ec8db20f21d0b8281
Vaibhav <vaibhavanandav@gmail.com>
2024-05-16 21:13:45 +0530

    Snake is a video game genre where the player maneuvers a growing line that becomes a primary obstacle to itself.

--- pl_java/exercise_3/Snake.java
+++ pl_java/exercise_3/Snake.java
@@ -2,32 +2,86 @@ package pl_java.exercise_3;
 
 public class Snake {
 
-/*
- * Attributes
- */
+    /*
+     * Attributes
+     */
+    private final String color;
+    private int length;
+    private String direction;
 
+    /*
+     * Constructors
+     */
+    public Snake(String color, int length, String direction) {
+        this.color = color;
+        this.length = length;
+        setDirection(direction);
+    }
 
+    public Snake(String direction) {
+        this.color = "Green";
+        this.length = 3;
+        setDirection(direction);
+    }
 
-/*
- * Constructors
- */
+    /*
+     * Getters
+     */
+    public String getColor() {
+        return color;
+    }
 
+    public int getLength() {
+        return length;
+    }
 
+    public String getDirection() {
+        return direction;
+    }
 
-/*
- * Getters
- */
+    /*
+     * Setters
+     */
+    private void setDirection(String direction) {
+        if (direction == "EAST" || direction == "WEST" || direction == "NORTH" || direction == "SOUTH") {
+            this.direction = direction;
+        } else {
+            System.out.println("Error! Enter valid direction");
+        }
+    }
 
+    /*
+     * Methods
+     */
+    public void modifyLength(int length) {
+        if(length<=0){
+            System.out.println("Error! Enter valid Length");
+        }
+        this.length=length;
+    }
 
+    public void turnLeft() {
+        if (direction == "EAST") {
+            setDirection("NORTH");
+        } else if (direction == "NORTH") {
+            setDirection("WEST");
+        } else if (direction == "WEST") {
+            setDirection("SOUTH");
+        } else {
+            setDirection("EAST");
+        }
+    }
 
-/*
- * Setters
- */
-
-
-
-/*
- * Methods
- */
+    public void turnRight() {
+        if (direction == "EAST") {
+            setDirection("SOUTH");
+        } else if (direction == "NORTH") {
+            setDirection("EAST");
+        } else if (direction == "WEST") {
+            setDirection("NORTH");
+        } else {
+            setDirection("WEST");
+        }
+    }
 
 }
