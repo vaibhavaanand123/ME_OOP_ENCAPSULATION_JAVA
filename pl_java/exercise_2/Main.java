ec8039f137b4e9c91415c20b1ceae0b91a5e2f75
Vaibhav <vaibhavanandav@gmail.com>
2024-05-16 20:41:55 +0530

    WhatsApp Messenger

--- pl_java/exercise_2/Main.java
+++ pl_java/exercise_2/Main.java
@@ -2,6 +2,7 @@ package pl_java.exercise_2;
 
 public class Main {
     public static void main(String args[]) {
+       
         System.out.println("Hello World!");
     }    
 }
--- pl_java/exercise_2/Profile.java
+++ pl_java/exercise_2/Profile.java
@@ -1,53 +1,106 @@
 package pl_java.exercise_2;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+
 public class Profile {
 
-/*
- * Attributes
- */
-    String name;
-    String countryCode;
-    String phoneNo;
-    String profilePhotoUrl;
-    String about;
-    String lastSeen;
+        /*
+         * Attributes
+         */
+        private String name;
+        private String countryCode;
+        private String phoneNo;
+        private URI profilePhotoUrl;
+        private String about;
+        private LocalDateTime lastSeen;
 
+        /*
+         * Constructors
+         */
+        public Profile(String name, String countryCode, String phoneNo, String profilePhotoUrl) throws URISyntaxException{
+                setName(name);
+                setPhoneNo(countryCode, phoneNo);
+                setProfilePhotoUrl(profilePhotoUrl);
+                this.lastSeen = LocalDateTime.now();
+        }
 
-/*
- * Constructors
- */
+        public Profile(String name, String countryCode, String phoneNo) throws URISyntaxException{
+                setName(name);
+                setPhoneNo(countryCode, phoneNo);
+                setProfilePhotoUrl("https://pps.whatsapp.net/v/noDp.jpg");
+                this.lastSeen = LocalDateTime.now();
+                setAbout("Hey there! I an using WhatsApp");
+        }
 
+        /*
+         * Getters
+         */
+        public String getName() {
+                return name;
+        }
 
-/*
- * Getters
- */
+        public String getCountryCode() {
+                return countryCode;
+        }
 
+        public String getPhonNo() {
+                return phoneNo;
+        }
 
-/*
- * Setters
- */
-    public void setName(String name){
-            this.name = name;
-    }
+        public String getProfilePhotoUrl() {
+                return profilePhotoUrl.toString();
+        }
 
-    public void setPhoneNo(String countryCode, String phoneNo){
-            this.countryCode = countryCode;
-            this.phoneNo = phoneNo;
-    }
+        public String getAbout() {
+                return about;
+        }
 
-    public void setProfilePhotoUrl(String profilePhotoUrl){
-        this.profilePhotoUrl = profilePhotoUrl;
-    }
+        public LocalDateTime getLastSeen() {
+                return lastSeen;
+        }
 
-    public void setAbout(String about){
-            this.about = about;
-    }
+        /*
+         * Setters
+         */
+        public void setName(String name) {
+                if (name.isBlank()) {
+                        System.out.println("Error!!: Name can't be blank");
+                } else {
+                        this.name = name;
+                }
+        }
 
+        public void setPhoneNo(String countryCode, String phoneNo) {
+                if (phoneNo.length() < 10) {
+                        System.out.println("Error!: Enter valid phone No.");
+                } else {
+                        this.countryCode = countryCode;
+                        this.phoneNo = phoneNo;
+                }
+        }
 
-/*
- * Methods
- */
-    public void updateLastSeen(String lastSeen){
-            this.lastSeen = lastSeen;
-    }
+        public void setProfilePhotoUrl(String profilePhotoUrl) throws URISyntaxException {
+                this.profilePhotoUrl = new URI(profilePhotoUrl);
+        }
+
+        public void setAbout(String about) {
+                if (about.isBlank()) {
+                        System.out.println("Error!: about cannot be Blank");
+                } else {
+                        this.about = about;
+                }
+        }
+
+        /*
+         * Methods
+         */
+        public void updateLastSeen(LocalDateTime lastSeen) {
+                if (this.lastSeen.isAfter(lastSeen)) {
+                        System.out.println("Error!: current lastseen cannot be more than input lastSeen!");
+                } else {
+                        this.lastSeen = lastSeen;
+                }
+        }
 }
